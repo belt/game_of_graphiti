53e4708947a973bd38e31e7d5cda6ec14373b37d
Paul Belt <paul.belt@gmail.com>
2021-07-07 10:50:21 -0400

    Fix bugs in default graphiti bootstrapping
    1. graphiti:install generates outdated cruft
    2. kaminari is an unstated expectation
    3. responders is an unstated expectation
    4. assume rails is running in pure API mode
    
    rough time: 20m of playing / googling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
+  register_exception Graphiti::Errors::RecordNotFound, status: 404
+
+  rescue_from Exception do |e|
+    handle_exception(e)
+  end
+
   include Graphiti::Rails::Responders
 end
--- config/application.rb
+++ config/application.rb
@@ -47,5 +47,8 @@ module CoreStrengths
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
+
+    # not running in pure API mode
+    config.debug_exception_response_format = :api
   end
 end
