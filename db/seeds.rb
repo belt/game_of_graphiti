6dfdcdbc85fefe9a7757b4a5b1a3982934dc293a
Paul Belt <paul.belt@gmail.com>
2021-07-07 14:51:38 -0400

    Provide seeds for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+root = User.create! first_name: "root", last_name: "account", email: "root@localhost"
+root_class = UserClass.create! user: root, name: "SMTP",
+                               start_date: Time.now.utc - 1.day,
+                               end_date: Time.now.utc + 1.day,
+                               description: "Demo class for root account",
+                               timezone: -5
